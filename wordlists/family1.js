c2d0c18db5912758b41c0c35c84cc27767feaa6d
wedg3 <lindgren@gmail.com>
2024-10-12 19:54:32 +0200

    Add files via upload

---
+++ wordlists/family1.js
@@ -0,0 +1,27 @@
+const wordlisttitle = "Family and body parts";
+
+const words = [
+	{ english: "mother", swedish: "mamma" },
+	{ english: "father", swedish: "pappa" },
+	{ english: "brother", swedish: "bror" },
+	{ english: "sister", swedish: "syster" },
+	{ english: "family", swedish: "familj" },
+	{ english: "grandmother", swedish: "mormor/farmor" },
+	{ english: "grandfather", swedish: "morfar/farfar" },
+	{ english: "son", swedish: "son" },
+	{ english: "daughter", swedish: "dotter" },
+	{ english: "cousin", swedish: "kusin" },
+	{ english: "head", swedish: "huvud" },
+	{ english: "arm", swedish: "arm" },
+	{ english: "hand", swedish: "hand" },
+	{ english: "leg", swedish: "ben" },
+	{ english: "foot", swedish: "fot" },
+	{ english: "eye", swedish: "öga" },
+	{ english: "ear", swedish: "öra" },
+	{ english: "nose", swedish: "näsa" },
+	{ english: "mouth", swedish: "mun" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
