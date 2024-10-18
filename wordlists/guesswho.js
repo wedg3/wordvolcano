51b473fac7aaf7a1b15fc7f31584f68ce11073a6
wedg3 <lindgren@gmail.com>
2024-10-18 10:02:08 +0200

    Create guesswho.js

---
+++ wordlists/guesswho.js
@@ -0,0 +1,38 @@
+const wordlisttitle = "Adjectives Guess Who";
+
+const words = [
+    { english: "tall", swedish: "lång" },
+    { english: "short", swedish: "kort" },
+    { english: "bald", swedish: "flintskallig" },
+    { english: "blonde", swedish: "blond" },
+    { english: "red", swedish: "röd" },
+    { english: "black", swedish: "svart" },
+    { english: "curly", swedish: "lockigt" },
+    { english: "straight", swedish: "rakt" },
+    { english: "wavy", swedish: "vågigt" },
+    { english: "blue", swedish: "blå" },
+    { english: "brown", swedish: "brun" },
+    { english: "green", swedish: "grön" },
+    { english: "beard", swedish: "skägg" },
+    { english: "mustache", swedish: "mustasch" },
+    { english: "young", swedish: "ung" },
+    { english: "old", swedish: "gammal" },
+    { english: "happy", swedish: "glad" },
+    { english: "sad", swedish: "ledsen" },
+    { english: "smiling", swedish: "leende" },
+    { english: "angry", swedish: "arg" },
+    { english: "big", swedish: "stor" },
+    { english: "small", swedish: "liten" },
+    { english: "freckled", swedish: "fräknig" },
+    { english: "dark", swedish: "mörk" },
+    { english: "light", swedish: "ljus" },
+    { english: "thin", swedish: "smal" },
+    { english: "round", swedish: "rund" },
+    { english: "hat", swedish: "hatt" },
+    { english: "earrings", swedish: "örhängen" },
+    { english: "long", swedish: "lång" },
+    { english: "short", swedish: "kort" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
