bebf1e61305b94694c9f7b182b68a58a0526a2d2
wedg3 <lindgren@gmail.com>
2024-11-05 07:59:54 +0100

    Create usdebate24.js

---
+++ wordlists/usdebate24.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "US Debate 2024";
+
+const words = [
+    { english: "believe", swedish: "tro" },
+    { english: "opportunity", swedish: "möjlighet" },
+    { english: "support", swedish: "stöd" },
+    { english: "terrible", swedish: "fruktansvärd" },
+    { english: "generous", swedish: "generös" },
+    { english: "destroyed", swedish: "förstörd" },
+    { english: "insane", swedish: "galen" },
+    { english: "pouring", swedish: "häller" },
+    { english: "rape", swedish: "våldtäkt" },
+    { english: "forced", swedish: "tvingad" },
+    { english: "pregnancy", swedish: "graviditet" },
+    { english: "violently", swedish: "våldsamt" },
+    { english: "crimes", swedish: "brott" },
+    { english: "weak", swedish: "svag" },
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
