04660deb290b463b8508b2d3deeebcc24f941631
wedg3 <lindgren@gmail.com>
2024-11-06 12:52:11 +0100

    Create holes1.js

---
+++ wordlists/holes1.js
@@ -0,0 +1,27 @@
+const wordlisttitle = "Words from Holes #1";
+
+const words = [
+    { english: "camp", swedish: "läger" },
+    { english: "shovel", swedish: "spade" },
+    { english: "hole", swedish: "hål" },
+    { english: "dig", swedish: "gräva" },
+    { english: "treasure", swedish: "skatt" },
+    { english: "lizard", swedish: "ödla" },
+    { english: "friend", swedish: "vän" },
+    { english: "family", swedish: "familj" },
+    { english: "curse", swedish: "förbannelse" },
+    { english: "desert", swedish: "öken" },
+    { english: "journey", swedish: "resa" },
+    { english: "justice", swedish: "rättvisa" },
+    { english: "onion", swedish: "lök" },
+    { english: "promise", swedish: "löfte" },
+    { english: "secret", swedish: "hemlighet" },
+    { english: "story", swedish: "berättelse" },
+    { english: "accuse", swedish: "anklaga" },
+    { english: "fate", swedish: "öde" },
+    { english: "legend", swedish: "legend" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
