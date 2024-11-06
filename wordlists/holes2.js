52308e14c4cd888f7a57d0a9cf3b5cfbba9818d3
wedg3 <lindgren@gmail.com>
2024-11-06 12:50:14 +0100

    Create holes2.js

---
+++ wordlists/holes2.js
@@ -0,0 +1,24 @@
+const wordlisttitle = "Words from Holes Level 2";
+
+const words = [
+    { english: "drought", swedish: "torka" },
+    { english: "mystery", swedish: "mysterium" },
+    { english: "persevere", swedish: "uthärda" },
+    { english: "fortune", swedish: "förmögenhet" },
+    { english: "wilderness", swedish: "vildmark" },
+    { english: "labor", swedish: "arbete" },
+    { english: "punishment", swedish: "straff" },
+    { english: "resolve", swedish: "beslutsamhet" },
+    { english: "reptile", swedish: "reptil" },
+    { english: "resilience", swedish: "motståndskraft" },
+    { english: "detention", swedish: "kvarsittning" },
+    { english: "redemption", swedish: "frälsning" },
+    { english: "excavation", swedish: "utgrävning" },
+    { english: "misfortune", swedish: "olycka" },
+    { english: "injustice", swedish: "orättvisa" },
+    { english: "inheritance", swedish: "arv" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
