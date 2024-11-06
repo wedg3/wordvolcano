b45a0770d062456774dd2dc66d7acc7130764a4f
wedg3 <lindgren@gmail.com>
2024-11-06 19:32:32 +0100

    Create deutsch1.js

---
+++ wordlists/deutsch1.js
@@ -0,0 +1,19 @@
+const wordlisttitle = "Deutsch 1";
+
+const words = [
+	{ english: "zumachen", swedish: "stänga" },
+	{ english: "mitkommen", swedish: "följa med" },
+	{ english: "herausholen", swedish: "ta fram" },
+	{ english: "wegfahren", swedish: "åka iväg" },
+
+{ english: "wegräumen", swedish: "städa undan" },
+	{ english: "sich anziehen", swedish: "klä på sig" },
+	{ english: "aufstehen", swedish: "gå upp" },
+	{ english: "aufmachen", swedish: "öppna" },
+
+	{ english: "einschlafen", swedish: "somna" },
+	{ english: "anrufen", swedish: "ringa" },
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
