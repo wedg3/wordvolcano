9161d5ca78d4619a76a4090110b83bd216815649
wedg3 <lindgren@gmail.com>
2024-11-08 10:12:56 +0100

    Create directions.js

---
+++ wordlists/directions.js
@@ -0,0 +1,34 @@
+const wordlisttitle = "Vägvisning / Directions";
+
+const words = [
+    { english: "Left", swedish: "vänster" },
+    { english: "Right", swedish: "höger" },
+    { english: "Up", swedish: "upp" },
+    { english: "Down", swedish: "ner" },
+    { english: "Forward", swedish: "framåt" },
+    { english: "Backward", swedish: "bakåt" },
+    { english: "North", swedish: "norr" },
+    { english: "South", swedish: "söder" },
+    { english: "East", swedish: "öster" },
+    { english: "West", swedish: "väster" },
+    { english: "Straight", swedish: "rakt" },
+    { english: "Behind", swedish: "bakom" },
+    { english: "Beside", swedish: "bredvid" },
+    { english: "over", swedish: "över" },
+    { english: "Under", swedish: "under" },
+    { english: "Around", swedish: "runt" },
+    { english: "Across", swedish: "tvärsöver" },
+    { english: "Near", swedish: "nära" },
+    { english: "Far", swedish: "långt" },
+    { english: "Opposite", swedish: "mittemot" },
+    { english: "Corner", swedish: "hörn" },
+    { english: "Between", swedish: "mellan" },
+    { english: "Past", swedish: "förbi" },
+    { english: "Beyond", swedish: "bortom" },
+    { english: "Inside", swedish: "inuti" },
+    { english: "Outside", swedish: "utanför" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
