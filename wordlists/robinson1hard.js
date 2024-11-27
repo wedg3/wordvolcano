33554749c9211a5d0d0b734399e3af5f0fd81e6a
wedg3 <lindgren@gmail.com>
2024-11-27 10:31:23 +0100

    Create robinson1hard.js

---
+++ wordlists/robinson1hard.js
@@ -0,0 +1,20 @@
+const wordlisttitle = "Robinson Crusoe #1 - Hard";
+
+const words = [
+    { english: "voyage", swedish: "resa" },
+    { english: "satisfy", swedish: "tillfredsställa" },
+    { english: "gale", swedish: "storm" },
+    { english: "frightened", swedish: "rädd" },
+    { english: "adventure", swedish: "äventyr" },
+    { english: "savage", swedish: "vild" },
+    { english: "cargo", swedish: "last" },
+    { english: "wrecked", swedish: "havererad/crashad" },
+    { english: "raging", swedish: "rasande" },
+    { english: "swallow", swedish: "svälja" },
+    { english: "shore", swedish: "strand" },
+    { english: "arose", swedish: "uppstod" },
+    { english: "exhausted", swedish: "utmattad" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
