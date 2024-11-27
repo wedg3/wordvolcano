feaf89fb152e8d0c6355372c7b1212c082bae2b7
wedg3 <lindgren@gmail.com>
2024-11-27 10:29:21 +0100

    Create robinson1easy.js

---
+++ wordlists/robinson1easy.js
@@ -0,0 +1,19 @@
+const wordlisttitle = "Robinson Crusoe #1 - Easier";
+
+const words = [
+    { english: "sailor", swedish: "sjöman" },
+    { english: "voyage", swedish: "resa" },
+    { english: "trade", swedish: "handel" },
+    { english: "satisfy", swedish: "tillfredsställa" },
+    { english: "frightened", swedish: "rädd" },
+    { english: "adventure", swedish: "äventyr" },
+    { english: "savage", swedish: "vild" },
+    { english: "slave", swedish: "slav" },
+    { english: "cargo", swedish: "last" },
+    { english: "wrecked", swedish: "havererad/crashad" },
+    { english: "swallow", swedish: "svälja" },
+    { english: "shore", swedish: "strand" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
