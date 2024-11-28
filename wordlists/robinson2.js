2bf9cfd013d477c429a3d0a5c80ebf2111d249f4
wedg3 <lindgren@gmail.com>
2024-11-28 07:45:23 +0100

    Create robinson2.js

---
+++ wordlists/robinson2.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "Robinson Crusoe #2";
+
+const words = [
+    { english: "shelter", swedish: "skydd" },
+    { english: "lodging", swedish: "boende" },
+    { english: "lonely", swedish: "ensam" },
+    { english: "raft", swedish: "flotte" },
+    { english: "cabin", swedish: "stuga" },
+    { english: "salvage", swedish: "bärga" },
+    { english: "timber", swedish: "timmer/virke" },
+    { english: "rope", swedish: "rep" },
+    { english: "dared", swedish: "vågade" },
+    { english: "explore", swedish: "utforska" },
+    { english: "visitor", swedish: "besökare" },
+    { english: "beast", swedish: "odjur" },
+    { english: "beak", swedish: "näbb" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
