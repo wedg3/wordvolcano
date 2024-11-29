5d731eea10e2a66ea20f5820ee22fff86f60761d
wedg3 <lindgren@gmail.com>
2024-11-29 10:15:13 +0100

    Create commonnouns2.js

---
+++ wordlists/commonnouns2.js
@@ -0,0 +1,23 @@
+const wordlisttitle = "Vanliga substantiv #2";
+
+const words = [
+    { english: "thing", swedish: "sak" },
+    { english: "woman", swedish: "kvinna" },
+    { english: "life", swedish: "liv" },
+    { english: "child", swedish: "barn" },
+    { english: "world", swedish: "värld" },
+    { english: "school", swedish: "skola" },
+    { english: "state", swedish: "stat" },
+    { english: "family", swedish: "familj" },
+    { english: "student", swedish: "student" },
+    { english: "group", swedish: "grupp" },
+    { english: "country", swedish: "land" },
+    { english: "part", swedish: "del" },
+    { english: "place", swedish: "plats" },
+    { english: "town", swedish: "stad" },
+    { english: "left", swedish: "vänster" },
+    { english: "right", swedish: "höger" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
