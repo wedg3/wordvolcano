3d30c5951e4bd6240416597a6017c1ac0dc56671
wedg3 <lindgren@gmail.com>
2024-11-29 10:30:57 +0100

    Create professions1.js

---
+++ wordlists/professions1.js
@@ -0,0 +1,24 @@
+const wordlisttitle = "Professions #1";
+
+const words = [
+    { english: "Astronaut", swedish: "Astronaut" },
+    { english: "Teacher", swedish: "Lärare" },
+    { english: "Doctor", swedish: "Läkare" },
+    { english: "Nurse", swedish: "Sjuksköterska" },
+    { english: "Engineer", swedish: "Ingenjör" },
+    { english: "Police", swedish: "Polis" },
+    { english: "Firefighter", swedish: "Brandman" },
+    { english: "Lawyer", swedish: "Advokat" },
+    { english: "Scientist", swedish: "Forskare" },
+    { english: "Farmer", swedish: "Bonde" },
+    { english: "Chef", swedish: "Kock" },
+    { english: "Mechanic", swedish: "Mekaniker" },
+    { english: "Pilot", swedish: "Pilot" },
+    { english: "Electrician", swedish: "Elektriker" },
+    { english: "Plumber", swedish: "Rörmokare" },
+    { english: "Dentist", swedish: "Tandläkare" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
