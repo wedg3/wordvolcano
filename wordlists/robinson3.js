7b4d5ef98f95cef1252f726034dc14ed7b3a6eaa
wedg3 <lindgren@gmail.com>
2024-12-02 14:53:22 +0100

    Create robinson3.js

---
+++ wordlists/robinson3.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "Robinson #3 12-15";
+
+const words = [
+    { english: "visitor", swedish: "besökare" },
+    { english: "beasts", swedish: "djur" },
+    { english: "beak", swedish: "näbb" },
+    { english: "treasure", swedish: "skatt" },
+    { english: "pieces", swedish: "bitar" },
+    { english: "bag", swedish: "väska" },
+    { english: "castle", swedish: "slott" },
+    { english: "fortress", swedish: "fästning" },
+    { english: "cellar", swedish: "källare" },
+    { english: "hunting", swedish: "jakt" },
+    { english: "goat", swedish: "get" },
+    { english: "flesh", swedish: "kött" },
+    { english: "dare", swedish: "våga" },
+    { english: "shrubs", swedish: "buskar" },
+    { english: "explore", swedish: "utforska" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
