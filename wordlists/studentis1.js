64e04e51f05c64e433a992da571ada9741b3debe
wedg3 <lindgren@gmail.com>
2024-12-09 23:17:12 +0100

    Create studentis1.js

---
+++ wordlists/studentis1.js
@@ -0,0 +1,27 @@
+const wordlisttitle = "The words of Isak #1";
+
+const words = [
+    { english: "steal", swedish: "stjäla" },
+    { english: "their", swedish: "deras" },
+    { english: "fishing", swedish: "fiske" },
+    { english: "middle", swedish: "mitt" },
+    { english: "suddenly", swedish: "plötsligt" },
+    { english: "heard", swedish: "hörde" },
+    { english: "noise", swedish: "ljud" },
+    { english: "there", swedish: "där" },
+    { english: "tomorrow", swedish: "imorgon" },
+    { english: "coconut", swedish: "kokosnöt" },
+    { english: "delicious", swedish: "utsökt" },
+    { english: "sea", swedish: "hav" },
+    { english: "filled", swedish: "fylld" },
+    { english: "cooked", swedish: "tillagad" },
+    { english: "signal", swedish: "signal" },
+    { english: "several", swedish: "flera" },
+    { english: "started", swedish: "började" },
+    { english: "threw", swedish: "kastade" },
+    { english: "arrived", swedish: "anlände" },
+    { english: "knew", swedish: "visste" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
