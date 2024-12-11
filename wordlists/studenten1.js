c9b9adba516749af0e1b9c2d67ab7d0d803b97b1
wedg3 <lindgren@gmail.com>
2024-12-11 15:41:11 +0100

    Create studenten1.js

---
+++ wordlists/studenten1.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "The "Words of Eddie #1";
+
+const words = [
+    { english: "watched", swedish: "tittade" },
+    { english: "character", swedish: "karaktär" },
+    { english: "gave", swedish: "gav" },
+    { english: "support", swedish: "stöd" },
+    { english: "them", swedish: "dem" },
+    { english: "beginning", swedish: "början" },
+    { english: "favorite", swedish: "favorit" },
+    { english: "rocket", swedish: "raket" },
+    { english: "give", swedish: "ge" },
+    { english: "recommend", swedish: "rekommendera" },
+    { english: "interesting", swedish: "intressant" },
+    { english: "recommend", swedish: "rekommendera" },
+    { english: "mountain", swedish: "berg" },
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
