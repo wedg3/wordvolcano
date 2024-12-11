3f337e5ea2bcaa11f3ec4ca355d0028360d93970
wedg3 <lindgren@gmail.com>
2024-12-11 15:44:34 +0100

    Create studenteb1.js

---
+++ wordlists/studenteb1.js
@@ -0,0 +1,27 @@
+const wordlisttitle = "Words of Ed #1";
+
+const words = [
+    { english: "called", swedish: "kallade" },
+    { english: "they", swedish: "de" },
+    { english: "them", swedish: "dem" },
+    { english: "prison", swedish: "fängelse" },
+    { english: "found", swedish: "fann" },
+    { english: "dried", swedish: "torkad" },
+    { english: "boring", swedish: "tråkig" },
+    { english: "others", swedish: "andra" },
+    { english: "shovel", swedish: "spade" },
+    { english: "desert", swedish: "öken" },
+    { english: "with", swedish: "med" },
+    { english: "their", swedish: "deras" },
+    { english: "showers", swedish: "duschar" },
+    { english: "recommend", swedish: "rekommendera" },
+    { english: "the", swedish: "den" },
+    { english: "there", swedish: "där" },
+    { english: "they", swedish: "de" },
+    { english: "happy", swedish: "glad" },
+    { english: "first", swedish: "först" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
