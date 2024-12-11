1d2cf9eb338b778a24b41f8e21a0bbdc75d104f3
wedg3 <lindgren@gmail.com>
2024-12-11 15:14:22 +0100

    Create studentbb1.js

---
+++ wordlists/studentbb1.js
@@ -0,0 +1,28 @@
+const wordlisttitle = "Words of Bonny #1";
+
+const words = [
+    { english: "built", swedish: "byggde" },
+    { english: "favorite", swedish: "favorit" },
+    { english: "about", swedish: "om" },
+    { english: "fourth", swedish: "fjärde" },
+    { english: "who", swedish: "vem" },
+    { english: "accused", swedish: "anklagade" },
+    { english: "steal", swedish: "stjäl" },
+    { english: "shoes", swedish: "skor" },
+    { english: "stole", swedish: "stal" },
+    { english: "arrested", swedish: "arresterad" },
+    { english: "deserve", swedish: "förtjänar" },
+    { english: "punished", swedish: "straffad" },
+    { english: "had", swedish: "hade" },
+    { english: "people", swedish: "människor" },
+    { english: "treasure", swedish: "skatt" },
+    { english: "grandpa", swedish: "morfar/farfar" },
+    { english: "trying", swedish: "försöker" },
+    { english: "find", swedish: "hitta" },
+    { english: "many", swedish: "många" },
+    { english: "can", swedish: "kan" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
