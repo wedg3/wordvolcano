8d2e79b4e49219245e031318d522a32b33c62659
wedg3 <lindgren@gmail.com>
2024-12-11 15:38:00 +0100

    Create studentak.js

---
+++ wordlists/studentak.js
@@ -0,0 +1,23 @@
+const wordlisttitle = "Words of Arvid #1";
+
+const words = [
+    { english: "title", swedish: "titel" },
+    { english: "comedy", swedish: "komedi" },
+    { english: "because", swedish: "eftersom" },
+    { english: "laughing", swedish: "skrattar" },
+    { english: "arrested", swedish: "arresterad" },
+    { english: "was", swedish: "var" },
+    { english: "poison", swedish: "gift" },
+    { english: "whole", swedish: "hel" },
+    { english: "treated", swedish: "behandlad" },
+    { english: "people", swedish: "människor" },
+    { english: "digging", swedish: "gräver" },
+    { english: "happening", swedish: "händer" },
+    { english: "happened", swedish: "hände" },
+    { english: "month", swedish: "månad" },
+    { english: "treasure", swedish: "skatt" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
