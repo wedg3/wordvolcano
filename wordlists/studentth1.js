6277b49d67cac7607e9792402a762f39649aa649
wedg3 <lindgren@gmail.com>
2024-12-13 13:04:41 +0100

    Create studentth1.js

---
+++ wordlists/studentth1.js
@@ -0,0 +1,13 @@
+const words = [
+    { english: "jumped", swedish: "hoppade" },
+    { english: "sounds", swedish: "ljud" },
+    { english: "engine", swedish: "motor" },
+    { english: "nothing", swedish: "ingenting" },
+    { english: "found", swedish: "hittade" },
+    { english: "something", swedish: "något" },
+    { english: "begins", swedish: "börjar" },
+    { english: "into", swedish: "in i" },
+    { english: "breakfast", swedish: "frukost" },
+    { english: "too salty", swedish: "för salt" },
+    { english: "finally", swedish: "äntligen" }
+];
