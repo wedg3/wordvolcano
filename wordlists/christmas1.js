5ce82cb7a9745a6736c0f5376032bd87596409a6
wedg3 <lindgren@gmail.com>
2024-12-13 10:25:54 +0100

    Create christmas1.js

---
+++ wordlists/christmas1.js
@@ -0,0 +1,26 @@
+const wordlisttitle = "Christmas words #1";
+
+const words = [
+    { english: "Christmas", swedish: "Jul" },
+    { english: "Santa", swedish: "Tomten" },
+    { english: "Snow", swedish: "Snö" },
+    { english: "Tree", swedish: "Träd" },
+    { english: "Gift", swedish: "Present/Gåva" },
+    { english: "Elf", swedish: "Tomtenisse" },
+    { english: "Star", swedish: "Stjärna" },
+    { english: "Bell", swedish: "Klocka" },
+    { english: "Merry", swedish: "Glad" },
+    { english: "Candy", swedish: "Godis" },
+    { english: "Sleigh", swedish: "Släde" },
+    { english: "Winter", swedish: "Vinter" },
+    { english: "Holly", swedish: "Järnek" },
+    { english: "Stocking", swedish: "Julstrumpa" },
+    { english: "Present", swedish: "Present" },
+    { english: "Chimney", swedish: "Skorsten" },
+    { english: "Reindeer", swedish: "Ren (djuret)" },
+    { english: "Cookie", swedish: "Kaka" }
+];
+
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
