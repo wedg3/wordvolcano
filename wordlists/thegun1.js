d1408ca99264a474bcbd9057681acf518327360e
wedg3 <lindgren@gmail.com>
2025-01-10 10:31:38 +0100

    Create thegun1.js

---
+++ wordlists/thegun1.js
@@ -0,0 +1,23 @@
+const wordlisttitle = "The Gun #1";
+
+const words = [
+    { english: "homework", swedish: "läxor" },
+    { english: "brother", swedish: "bror" },
+    { english: "window", swedish: "fönster" },
+    { english: "real", swedish: "verklig" },
+    { english: "fingerprints", swedish: "fingeravtryck" },
+    { english: "fire", swedish: "eld" },
+    { english: "mistake", swedish: "misstag" },
+    { english: "loaded", swedish: "laddad" },
+    { english: "ignore", swedish: "strunta i" },
+    { english: "stressed", swedish: "stressad" },
+    { english: "swear", swedish: "svära" },
+    { english: "explain", swedish: "förklara" },
+    { english: "drunk", swedish: "full/berusad" },
+    { english: "adventure", swedish: "äventyr" },
+    { english: "mean", swedish: "elak" },
+    { english: "gun", swedish: "pistol" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
