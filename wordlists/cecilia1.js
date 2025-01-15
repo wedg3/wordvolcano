8264660696b227c9d9e4d5a210a5be7af2479135
wedg3 <lindgren@gmail.com>
2025-01-15 13:16:31 +0100

    Create cecilia1.js

---
+++ wordlists/cecilia1.js
@@ -0,0 +1,23 @@
+
+const wordlisttitle = "Nelson Mandela";
+
+const words = [
+	{ english: "basically", swedish: "i grund och botten" },
+	{ english: "legal", swedish: "laglig" },
+	{ english: "residential area", swedish: "bostadsområde" },
+	{ english: "enforce", swedish: "upprätthålla" },
+	{ english: "maid", swedish: "hembiträde" },
+  	{ english: "split", swedish: "dela" },
+	{ english: "ruler", swedish: "härskare" },
+	{ english: "wooden hut", swedish: "trähydda" },
+	{ english: "ban", swedish: "förbjuda" },
+	{ english: "force", swedish: "makt/kraft" },
+  	{ english: "poverty", swedish: "fattigdom" },
+	{ english: "township", swedish: "bosättningsområde för svarta" },
+	{ english: "segregate", swedish: "särskilja" },
+	{ english: "population", swedish: "befolkning" },
+	{ english: "separate", swedish: "särskilja" },
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
