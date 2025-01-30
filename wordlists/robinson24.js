7eb736b750f25f0098f06c7a744c1756c2cfb322
wedg3 <lindgren@gmail.com>
2025-01-30 07:57:59 +0100

    Create robinson24.js

---
+++ wordlists/robinson24.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "Change this title";
+
+const words = [
+    { english: "Grain", swedish: "säd" },
+    { english: "Barley", swedish: "korn" },
+    { english: "Harvest", swedish: "skörd" },
+    { english: "Journey", swedish: "resa" },
+    { english: "Parrot", swedish: "papegoja" },
+    { english: "Fence", swedish: "staket" },
+    { english: "Stakes", swedish: "pålar" },
+    { english: "Basket", swedish: "korg" },
+    { english: "Humble", swedish: "Ödmjuk (oskrytsam)" },
+    { english: "Clay", swedish: "lera" },
+    { english: "Vessel", swedish: "kärl" },
+    { english: "Canoe", swedish: "kanot" },
+    { english: "Tools", swedish: "verktyg" },
+    { english: "Castle", swedish: "slott/borg" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
