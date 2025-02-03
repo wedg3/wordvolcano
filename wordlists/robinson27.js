5060e37dff35b559d182bb5c5a7389d45c538ea1
wedg3 <lindgren@gmail.com>
2025-02-03 15:33:30 +0100

    Update robinson27.js

--- wordlists/robinson27.js
+++ wordlists/robinson27.js
@@ -1,20 +1,20 @@
 const wordlisttitle = "Robinson 27-29";
 
 const words = [
-    { english: "Umbrella", swedish: "paraply" },
-    { english: "Shed", swedish: "skjul" },
-    { english: "Adventure", swedish: "äventyr" },
-    { english: "Savage", swedish: "vilde" },
-    { english: "Escape", swedish: "flykt" },
-    { english: "Voice", swedish: "röst" },
-    { english: "Distress", swedish: "nöd" },
-    { english: "Cliff", swedish: "klippa" },
-    { english: "Feeble", swedish: "svag/klen" },
-    { english: "Stir", swedish: "röra" },
-    { english: "Proud", swedish: "stolt" },
-    { english: "Clumsy", swedish: "klumpig" },
-    { english: "Beard", swedish: "skägg" },
-    { english: "Stroll", swedish: "promenad" }
+    { english: "umbrella", swedish: "paraply" },
+    { english: "shed", swedish: "skjul" },
+    { english: "adventure", swedish: "äventyr" },
+    { english: "savage", swedish: "vilde" },
+    { english: "escape", swedish: "flykt" },
+    { english: "voice", swedish: "röst" },
+    { english: "distress", swedish: "nöd" },
+    { english: "cliff", swedish: "klippa" },
+    { english: "feeble", swedish: "svag/klen" },
+    { english: "stir", swedish: "röra" },
+    { english: "proud", swedish: "stolt" },
+    { english: "clumsy", swedish: "klumpig" },
+    { english: "beard", swedish: "skägg" },
+    { english: "stroll", swedish: "promenad" }
 ];
 
 document.getElementById("dynamic-heading").textContent = wordlisttitle;
