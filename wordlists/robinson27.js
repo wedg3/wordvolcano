b8456a1a24c2b0905bacdab519705d84a644b76f
wedg3 <lindgren@gmail.com>
2025-02-03 14:30:17 +0100

    Create robinson27.js

---
+++ wordlists/robinson27.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "Robinson 27-29";
+
+const words = [
+    { english: "Umbrella", swedish: "paraply" },
+    { english: "Shed", swedish: "skjul" },
+    { english: "Adventure", swedish: "äventyr" },
+    { english: "Savage", swedish: "vilde" },
+    { english: "Escape", swedish: "flykt" },
+    { english: "Voice", swedish: "röst" },
+    { english: "Distress", swedish: "nöd" },
+    { english: "Cliff", swedish: "klippa" },
+    { english: "Feeble", swedish: "svag/klen" },
+    { english: "Stir", swedish: "röra" },
+    { english: "Proud", swedish: "stolt" },
+    { english: "Clumsy", swedish: "klumpig" },
+    { english: "Beard", swedish: "skägg" },
+    { english: "Stroll", swedish: "promenad" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
