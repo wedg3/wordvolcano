9c4cd59430f5d53a0cdcd95bae272f08d34f2df6
wedg3 <lindgren@gmail.com>
2025-02-06 07:14:34 +0100

    Create robinson8.js

---
+++ wordlists/robinson8.js
@@ -0,0 +1,17 @@
+const wordlisttitle = "Robinson Part 8";
+
+const words = [
+    { english: "smooth", swedish: "slät" },
+    { english: "dough", swedish: "deg" },
+    { english: "prosperous", swedish: "välmående/rik" },
+    { english: "savage", swedish: "vilde" },
+    { english: "cattle", swedish: "boskap" },
+    { english: "mill", swedish: "kvarn" },
+    { english: "bold", swedish: "djärv (modig)" },
+    { english: "feast", swedish: "festmåltid" },
+    { english: "tremble", swedish: "darra" },
+    { english: "quiet", swedish: "tyst" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
