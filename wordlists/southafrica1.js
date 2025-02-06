60c9636ac3356d82a8b831eee7244d335dbf4fe8
wedg3 <lindgren@gmail.com>
2025-02-06 15:26:35 +0100

    Create southafrica1.js

---
+++ wordlists/southafrica1.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "South Africa: Difficult words #1";
+
+const words = [
+    { english: "apartheid", swedish: "Sydafrikas gamla raspolitik" },
+    { english: "segregation", swedish: "avskiljning (av människor)" },
+    { english: "discrimination", swedish: "diskriminering" },
+    { english: "oppression", swedish: "förtryck" },
+    { english: "reconciliation", swedish: "försoning" },
+    { english: "inequality", swedish: "ojämlikhet" },
+    { english: "revolution", swedish: "revolution" },
+    { english: "imprisonment", swedish: "fängslande (vid brott)" },
+    { english: "constitution", swedish: "grundlag" },
+    { english: "separate", swedish: "separera" },
+    { english: "basically", swedish: "i grund och botten" },
+    { english: "overcome", swedish: "övervinna" },
+    { english: "abuse", swedish: "missbruk" },
+    { english: "dignity", swedish: "värdighet" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
