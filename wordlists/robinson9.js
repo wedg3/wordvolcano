0b452fd01da8e7954cdb2ec593049c80e2ac95db
wedg3 <lindgren@gmail.com>
2025-02-11 09:04:36 +0100

    Create robinson9.js

---
+++ wordlists/robinson9.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "Robinson Crusoe: Part 9";
+
+const words = [
+    { english: "blaze", swedish: "flamma" },
+    { english: "visitor", swedish: "besökare" },
+    { english: "chamber", swedish: "kammare" },
+    { english: "cave", swedish: "grotta" },
+    { english: "poultry", swedish: "fjärderfä (höns etc" },
+    { english: "encounter", swedish: "möte" },
+    { english: "dreadful", swedish: "fruktansvärd" },
+    { english: "torch", swedish: "fackla" },
+    { english: "shelter", swedish: "skydd" },
+    { english: "shaggy", swedish: "rufsig" },
+    { english: "cannon", swedish: "kanon" },
+    { english: "spyglass", swedish: "kikare" },
+    { english: "hollow", swedish: "hålighet" },
+    { english: "moan", swedish: "stöna" },
+    { english: "feast", swedish: "festmåltid" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
