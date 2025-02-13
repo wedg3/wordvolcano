6e7aa32f75caed6635d9329df74b353d0a38a401
wedg3 <lindgren@gmail.com>
2025-02-13 07:12:51 +0100

    Create robinson11.js

---
+++ wordlists/robinson11.js
@@ -0,0 +1,24 @@
+const wordlisttitle = "Robinson Crusoe: Part 11";
+
+const words = [
+    { english: "Castle", swedish: "Slott" },
+    { english: "Canoe", swedish: "Kanot" },
+    { english: "Shore", swedish: "Strand" },
+    { english: "Pursuer", swedish: "Förföljare" },
+    { english: "Prisoner", swedish: "Fånge" },
+    { english: "Trembling", swedish: "Darrande" },
+    { english: "Captive", swedish: "Fånge" },
+    { english: "Beckon", swedish: "Vinka hit" },
+    { english: "Signs", swedish: "Tecken" },
+    { english: "Pleased", swedish: "Nöjd" },
+    { english: "Handsome", swedish: "Stilig" },
+    { english: "Forehead", swedish: "Panna" },
+    { english: "Olive", swedish: "Oliv" },
+    { english: "Master", swedish: "Herre" },
+    { english: "Spyglass", swedish: "Kikare" },
+    { english: "Awoke", swedish: "Vaknade" },
+    { english: "Faithful", swedish: "Trogen" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
