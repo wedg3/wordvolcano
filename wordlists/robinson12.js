9d709518616550c94c772ca1bed6c068b0cb6be0
wedg3 <lindgren@gmail.com>
2025-02-18 07:23:39 +0100

    Create robinson12.js

---
+++ wordlists/robinson12.js
@@ -0,0 +1,24 @@
+const wordlisttitle = "Robinson Crusoe: Part 12";
+
+const words = [
+    { english: "pity", swedish: "medlidande" },
+    { english: "amazed", swedish: "förbluffad" },
+    { english: "broth", swedish: "buljong" },
+    { english: "hatchet", swedish: "handyxa" },
+    { english: "mainland", swedish: "fastland" },
+    { english: "tame", swedish: "tam" },
+    { english: "therefore", swedish: "därför" },
+    { english: "chisel", swedish: "mejsel" },
+    { english: "stitching", swedish: "syr" },
+    { english: "carpenter", swedish: "snickare" },
+    { english: "dock", swedish: "kaj" },
+    { english: "tide", swedish: "tidvatten" },
+    { english: "branches", swedish: "grenar" },
+    { english: "thatched", swedish: "halmtäckt" },
+    { english: "occupied", swedish: "upptagen" },
+    { english: "creator", swedish: "skapare" },
+    { english: "worship", swedish: "dyrka/tillbe" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
