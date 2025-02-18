88163912e2a8b2a8ad63c7bd12fe8b13f1b23bb2
wedg3 <lindgren@gmail.com>
2025-02-18 15:49:13 +0100

    Create robinson13.js

---
+++ wordlists/robinson13.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "Robinson Crusoe: Part 13";
+
+const words = [
+    { english: "wonderful", swedish: "fantastiskt" },
+    { english: "haste", swedish: "brådska" },
+    { english: "strange", swedish: "konstig, främmande" },
+    { english: "swiftly", swedish: "snabbt" },
+    { english: "shore", swedish: "strand, kust" },
+    { english: "anchor", swedish: "ankare" },
+    { english: "distress", swedish: "nöd, ångest" },
+    { english: "despair", swedish: "förtvivlan" },
+    { english: "scattered", swedish: "utspridda" },
+    { english: "tide", swedish: "tidvatten" },
+    { english: "seized", swedish: "tog, beslagtog" },
+    { english: "pirates", swedish: "pirater" },
+    { english: "firearms", swedish: "skjutvapen" },
+    { english: "overcome", swedish: "övervinna" },
+    { english: "rescue", swedish: "rädda, räddning" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
