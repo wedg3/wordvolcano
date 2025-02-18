94ccf24415cffab14de6719609495bf1a23f48ab
wedg3 <lindgren@gmail.com>
2025-02-18 12:57:33 +0100

    Create parttimeindian1.js

---
+++ wordlists/parttimeindian1.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "Diary of a part time indian: Easy version";
+
+const words = [
+    { english: "Reservation", swedish: "reservat" },
+    { english: "Tribe", swedish: "stam" },
+    { english: "Basketball", swedish: "basketboll" },
+    { english: "Friendship", swedish: "vänskap" },
+    { english: "Bully", swedish: "mobbare" },
+    { english: "Poverty", swedish: "fattigdom" },
+    { english: "Dreams", swedish: "drömmar" },
+    { english: "Hope", swedish: "hopp" },
+    { english: "Identity", swedish: "identitet" },
+    { english: "Family", swedish: "familj" },
+    { english: "School", swedish: "skola" },
+    { english: "Journey", swedish: "resa" },
+    { english: "Cartoon", swedish: "tecknad serie" },
+    { english: "Laugh", swedish: "skratta" },
+    { english: "Courage", swedish: "mod" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
