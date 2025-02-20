96943df9929f4bd065654424ee9f651e19392a1a
wedg3 <lindgren@gmail.com>
2025-02-20 07:19:52 +0100

    Create robinson1415.js

---
+++ wordlists/robinson1415.js
@@ -0,0 +1,21 @@
+const wordlisttitle = "Robinson part 14-15. The final chapters";
+
+const words = [
+    { english: "Anxious", swedish: "Orolig" },
+    { english: "Powder", swedish: "Pulver" },
+    { english: "Stir", swedish: "Röra" },
+    { english: "Mercy", swedish: "Barmhärtighet" },
+    { english: "Begged", swedish: "Bönföll" },
+    { english: "Rejoice", swedish: "Hurra/Jubla" },
+    { english: "Separated", swedish: "Separerade" },
+    { english: "Musket", swedish: "Musköt" },
+    { english: "Eagerness", swedish: "Ivrighet" },
+    { english: "Companions", swedish: "Kamrater" },
+    { english: "Trustworthy", swedish: "Pålitlig" },
+    { english: "Captured", swedish: "Tillfångatagen" },
+    { english: "Victory", swedish: "Seger" },
+    { english: "Surprise", swedish: "Överraskning" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
