8f47f3c63eacda91bc658a225b129a20adef23da
wedg3 <lindgren@gmail.com>
2025-03-12 15:22:04 +0100

    Create castaway.js

---
+++ wordlists/castaway.js
@@ -0,0 +1,25 @@
+const wordlisttitle = "Cast Away Movie";
+
+const words = [
+    { english: "Beach", swedish: "Strand" },
+    { english: "Raft", swedish: "Flotte" },
+    { english: "Cave", swedish: "Grotta" },
+    { english: "Tide", swedish: "Tidvatten" },
+    { english: "Shelter", swedish: "Skydd" },
+    { english: "Survive", swedish: "Överleva" },
+    { english: "Escape", swedish: "Fly" },
+    { english: "Climb", swedish: "Klättra" },
+    { english: "Mountain", swedish: "Berg" },
+    { english: "Endure", swedish: "Uthärda" },
+    { english: "Deliver", swedish: "Leverera" },
+    { english: "Rope", swedish: "Rep" },
+    { english: "Horizon", swedish: "Horisont" },
+    { english: "Rescue", swedish: "Rädda" },
+    { english: "Isolation", swedish: "Isolering" },
+    { english: "Bury", swedish: "Begrava" },
+    { english: "Faint", swedish: "Svimma" },
+    { english: "Flashlight", swedish: "Ficklampa" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
