fe6081a11a31c9dc96f0a16d0622125faa1828a5
wedg3 <lindgren@gmail.com>
2025-04-15 23:38:46 +0200

    Create easter2.js

---
+++ wordlists/easter2.js
@@ -0,0 +1,19 @@
+const wordlisttitle = "Easter #2";
+
+const words = [
+    { english: "unique", swedish: "unik" },
+    { english: "witches", swedish: "häxor" },
+    { english: "cheeks", swedish: "kinder" },
+    { english: "hymns", swedish: "psalmer" },
+    { english: "birch", swedish: "björk" },
+    { english: "twigs", swedish: "kvistar" },
+    { english: "native", swedish: "infödd" },
+    { english: "bonfires", swedish: "brasor" },
+    { english: "pickled", swedish: "inlagd" },
+    { english: "herring", swedish: "strömming (pickled herring = sill)" },
+    { english: "attend", swedish: "delta" },
+    { english: "exchange", swedish: "byta" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
