e638695f4b2b3a541b17901033cc660bc9c14d07
wedg3 <lindgren@gmail.com>
2025-04-15 23:37:35 +0200

    Create easter1.js

---
+++ wordlists/easter1.js
@@ -0,0 +1,20 @@
+const wordlisttitle = "Easter #1";
+
+const words = [
+    { english: "medieval", swedish: "medeltida" },
+    { english: "receive", swedish: "ta emot" },
+    { english: "faith", swedish: "tro" },
+    { english: "existing", swedish: "existerande" },
+    { english: "adopt", swedish: "anta / införa" },
+    { english: "pagan", swedish: "hednisk (icke-kristen)" },
+    { english: "origin", swedish: "ursprung" },
+    { english: "rebirth", swedish: "återfödelse" },
+    { english: "fertility", swedish: "fruktbarhet" },
+    { english: "crucifixion", swedish: "korsfästelse" },
+    { english: "ancient", swedish: "forntida" },
+    { english: "associated", swedish: "förknippad" },
+    { english: "resurrection", swedish: "uppståndelse" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
