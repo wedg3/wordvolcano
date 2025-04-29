6ca3c656a6b9e846679428b6a33c1bcf6199d4a9
wedg3 <lindgren@gmail.com>
2025-04-29 08:37:17 +0200

    Create fathersday.js

---
+++ wordlists/fathersday.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "This American Life: Father's Day";
+
+const words = [
+    { english: "trash", swedish: "skräp" },
+    { english: "remember", swedish: "komma ihåg" },
+    { english: "laugh", swedish: "skratta" },
+    { english: "surprise", swedish: "överraskning" },
+    { english: "promise", swedish: "löfte" },
+    { english: "search", swedish: "söka" },
+    { english: "recycling", swedish: "återvinning" },
+    { english: "frustrated", swedish: "frustrerad" },
+    { english: "expression", swedish: "uttryck" },
+    { english: "intention", swedish: "avsikt" },
+    { english: "emotions", swedish: "känslor" },
+    { english: "disappear", swedish: "försvinna" },
+    { english: "knock", swedish: "knacka" },
+    { english: "weird", swedish: "konstig" },
+    { english: "dump", swedish: "dumpa" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
