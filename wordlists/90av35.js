42a0b4af084ff40bcf83dc6e562fa1658812d1d5
wedg3 <lindgren@gmail.com>
2025-08-27 08:42:30 +0200

    Create 90av35.js

---
+++ wordlists/90av35.js
@@ -0,0 +1,22 @@
+const wordlisttitle = "English-Swedish Wordlist";
+
+const words = [
+    { english: "reach out", swedish: "vända sig till" },
+    { english: "gender", swedish: "kön (socialt)" },
+    { english: "sex", swedish: "kön (biologiskt)" },
+    { english: "assumption", swedish: "antagande" },
+    { english: "recent", swedish: "nyligen" },
+    { english: "research", swedish: "forskning" },
+    { english: "apparently", swedish: "tydligen" },
+    { english: "among", swedish: "bland" },
+    { english: "on behalf of", swedish: "å någons vägnar" },
+    { english: "extend", swedish: "utvidga" },
+    { english: "issue", swedish: "problem" },
+    { english: "insecure", swedish: "osäker" },
+    { english: "distorted", swedish: "förvriden" },
+    { english: "compel", swedish: "tvinga" },
+    { english: "current", swedish: "nuvarande" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
