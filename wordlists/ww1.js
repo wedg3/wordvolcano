48966bbd3c1f2a0dfe5af54f1a6e73078c153d0a
wedg3 <lindgren@gmail.com>
2025-08-27 09:40:54 +0200

    Create ww1.js

---
+++ wordlists/ww1.js
@@ -0,0 +1,26 @@
+const wordlisttitle = "World War 1 1914-1918";
+
+const words = [
+    { english: "uncanny", swedish: "kuslig" },
+    { english: "casualty", swedish: "olycks" },
+    { english: "relieved", swedish: "lättad" },
+    { english: "retire", swedish: "gå i pension" },
+    { english: "demolished", swedish: "riven" },
+    { english: "reinforcing", swedish: "förstärker" },
+    { english: "trench", swedish: "skyttegrav" },
+    { english: "thud", swedish: "duns" },
+    { english: "wounded", swedish: "sårad" },
+    { english: "desolate", swedish: "öde" },
+    { english: "shell", swedish: "granat" },
+    { english: "burst", swedish: "sprängas" },
+    { english: "flashes", swedish: "blixtar" },
+    { english: "scream", swedish: "skrik" },
+    { english: "encouraging", swedish: "uppmuntrande" },
+    { english: "inhabited", swedish: "bebodd" },
+    { english: "pride", swedish: "stolthet" },
+    { english: "regarding", swedish: "angående" },
+    { english: "causes", swedish: "orsaker" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
