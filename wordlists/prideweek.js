ee3877291222fe3c9f8e521d369441f9d06da7c0
wedg3 <lindgren@gmail.com>
2025-08-27 08:48:59 +0200

    Create prideweek.js

---
+++ wordlists/prideweek.js
@@ -0,0 +1,24 @@
+const wordlisttitle = "Prideweek";
+
+const words = [
+    { english: "mine", swedish: "gruva" },
+    { english: "employ", swedish: "anställa, sysselsätta" },
+    { english: "pit", swedish: "grop, hål" },
+    { english: "nationwide", swedish: "landsomfattande" },
+    { english: "strike", swedish: "strejk" },
+    { english: "significant", swedish: "avgörande, betydande" },
+    { english: "benefit", swedish: "understöd" },
+    { english: "union", swedish: "fackförening" },
+    { english: "funds", swedish: "kapital" },
+    { english: "determination", swedish: "övertygelse, beslutsamhet" },
+    { english: "lightbulb", swedish: "glödlampa" },
+    { english: "prejudice", swedish: "fördom" },
+    { english: "valley", swedish: "dal" },
+    { english: "handshake", swedish: "handslag" },
+    { english: "offend", swedish: "förolämpa" },
+    { english: "ridicule", swedish: "förlöjliga" },
+    { english: "badge", swedish: "märke" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
