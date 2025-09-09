1837f49c708ae9497eaae557965a0867c9f8e4a5
wedg3 <lindgren@gmail.com>
2025-09-09 23:00:50 +0200

    Create betweenwars.js

---
+++ wordlists/betweenwars.js
@@ -0,0 +1,25 @@
+const wordlisttitle = "Between the wars. The 20s and 30s.";
+
+const words = [
+    { english: "treaty", swedish: "fördrag" },
+    { english: "armistice", swedish: "vapenstillestånd" },
+    { english: "disarm", swedish: "avväpna" },
+    { english: "stock market", swedish: "aktiemarknad" },
+    { english: "prosperity", swedish: "välstånd" },
+    { english: "fashion", swedish: "mode" },
+    { english: "skyscraper", swedish: "skyskrapa" },
+    { english: "automobile", swedish: "bil" },
+    { english: "prohibition", swedish: "förbudstiden (om alkohol)" },
+    { english: "bootlegger", swedish: "smugglare" },
+    { english: "depression", swedish: "depression" },
+    { english: "strike", swedish: "strejk" },
+    { english: "unemployment", swedish: "arbetslöshet" },
+    { english: "poverty", swedish: "fattigdom" },
+    { english: "Dust Bowl", swedish: "Dust Bowl" },
+    { english: "drought", swedish: "torka" },
+    { english: "assembly line", swedish: "monteringslinje" },
+    { english: "appeasement", swedish: "eftergiftspolitik" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
