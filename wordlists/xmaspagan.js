1181fc95d89ea3c103fcab716aa1df8d262ac41d
wedg3 <lindgren@gmail.com>
2025-12-09 08:05:11 +0100

    Fix wordlist title in xmaspagan.js

---
+++ wordlists/xmaspagan.js
@@ -0,0 +1,27 @@
+const wordlisttitle = "Origins of Christmas;
+
+const words = [
+    { english: "agriculture", swedish: "jordbruk" },
+    { english: "blended", swedish: "sammanblandad" },
+    { english: "brought", swedish: "hämtat" },
+    { english: "celebrate", swedish: "fira" },
+    { english: "christianity", swedish: "kristendom" },
+    { english: "communities", swedish: "samhällen" },
+    { english: "customs", swedish: "traditioner / sedvänja" },
+    { english: "decorate", swedish: "dekorera" },
+    { english: "evergreen", swedish: "vintergröna / barrväxt" },
+    { english: "feast", swedish: "festmåltid" },
+    { english: "hope", swedish: "hopp" },
+    { english: "influence", swedish: "påverkan" },
+    { english: "log", swedish: "stock" },
+    { english: "origin", swedish: "ursprung" },
+    { english: "pagan", swedish: "hednisk (före kristendomen)" },
+    { english: "rituals", swedish: "ritualer" },
+    { english: "season", swedish: "säsong" },
+    { english: "solstice", swedish: "(vinter)solstånd" },
+    { english: "yule", swedish: "julhögtid (fornnordisk festival)" },
+    { english: "regardless", swedish: "oavsett" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
