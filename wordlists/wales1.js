878c7397af6a8ba2a3c46494f9cd38ee27b728ab
wedg3 <lindgren@gmail.com>
2026-01-14 08:55:08 +0100

    Add Welsh-English wordlist for Wales 1

---
+++ wordlists/wales1.js
@@ -0,0 +1,46 @@
+const wordlisttitle = "Straight-line through Wales 1";
+
+const words = [
+    { english: "mischief", swedish: "bus" },
+    { english: "adventure", swedish: "äventyr" },
+    { english: "attempt", swedish: "försök" },
+    { english: "remote", swedish: "långt borta" },
+    { english: "estuary", swedish: "flodmynning" },
+    { english: "barbed", swedish: "taggig (som taggtråd)" },
+    { english: "hedgerow", swedish: "häck" },
+    { english: "gorge", swedish: "ravin" },
+    { english: "marshes", swedish: "träsk" },
+    { english: "route", swedish: "rutt/väg" },
+    { english: "crowded", swedish: "trång" },
+    { english: "camp", swedish: "slå läger" },
+    { english: "disguise", swedish: "förklädnad" },
+    { english: "dreading", swedish: "vara rädd för" },
+    { english: "territory", swedish: "område" },
+    { english: "terrified", swedish: "jätterädd" },
+    { english: "obstacle", swedish: "hinder" },
+    { english: "dreaded", swedish: "mycket obehaglig" },
+    { english: "torrent", swedish: "stark ström" },
+    { english: "swift", swedish: "snabb" },
+    { english: "downstream", swedish: "nedströms" },
+    { english: "gauge", swedish: "bedöma" },
+    { english: "straying", swedish: "gå fel/avvika" },
+    { english: "clutching", swedish: "hålla hårt" },
+    { english: "submerged", swedish: "under vatten" },
+    { english: "devastated", swedish: "väldigt ledsen" },
+    { english: "collapsed", swedish: "rasad" },
+    { english: "precarious", swedish: "riskfyllt" },
+    { english: "perched", swedish: "sitta högt" },
+    { english: "sanctuary", swedish: "trygg plats" },
+    { english: "relieve", swedish: "lätta" },
+    { english: "alert", swedish: "uppmärksam" },
+    { english: "exposed", swedish: "oskyddad" },
+    { english: "daunting", swedish: "skrämmande" },
+    { english: "improvise", swedish: "hitta på i stunden" },
+    { english: "abandoned", swedish: "övergiven" },
+    { english: "contend with", swedish: "klara av" },
+    { english: "deviate", swedish: "avvika/ändra väg" },
+    { english: "obstacles", swedish: "hinder" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
