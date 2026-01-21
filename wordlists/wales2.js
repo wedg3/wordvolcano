b6b3f89693ab0b870af3ef0444a49af0f92ce01b
wedg3 <lindgren@gmail.com>
2026-01-21 09:49:33 +0100

    Add Wales 2 wordlist with English-Swedish translations

---
+++ wordlists/wales2.js
@@ -0,0 +1,27 @@
+    const wordlisttitle = "Straight-line through Wales 2";
+
+const words = [
+
+	{ english: "marshes", swedish: "träsk" },
+    { english: "route", swedish: "rutt/väg" },
+    { english: "crowded", swedish: "trång" },
+    { english: "camp", swedish: "slå läger" },
+    { english: "disguise", swedish: "förklädnad" },
+    { english: "dreading", swedish: "oroa sig för" },
+    { english: "territory", swedish: "område" },
+    { english: "terrified", swedish: "jätterädd" },
+    { english: "obstacle", swedish: "hinder" },
+    { english: "dreaded", swedish: "mycket obehaglig" },
+    { english: "torrent", swedish: "stark ström" },
+    { english: "swift", swedish: "snabb" },
+    { english: "downstream", swedish: "nedströms" },
+    { english: "gauge", swedish: "bedöma" },
+    { english: "straying", swedish: "gå fel/avvika" },
+    { english: "clutching", swedish: "hålla hårt" },
+    { english: "submerged", swedish: "under vatten" }
+];
+
+document.getElementById("dynamic-heading").textContent = wordlisttitle;
+document.getElementById("dynamic-ending").textContent = wordlisttitle;
+	
+	
